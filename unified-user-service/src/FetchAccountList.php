459364de6af9fb1b5da3bbf6b86ca0b204f53a1c
Mateusz Sip <mateusz.sip@gmail.com>
2022-08-09 10:55:31 +0200

    Fetch accounts from cards

---
+++ unified-user-service/src/FetchAccountList.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final class FetchAccountList
+{
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client, private readonly string $product, string $baseUri)
+    {
+        $this->client = $client->withOptions([
+            'base_uri' => $baseUri,
+        ]);
+    }
+
+    /** @return Account[] */
+    public function __invoke(): array
+    {
+        $response = $this->client->request(
+            'GET',
+            '/accounts'
+        );
+
+        $payload = $response->toArray();
+
+
+        return array_map(
+            fn(array $account) => new Account($account['id'], $this->product),
+            $payload
+        );
+    }
+}
